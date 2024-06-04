ce7c090b0d22c6d733ee727276c7eb1674135c86
Aditya Yadav <adityayadav44556@gmail.com>
2024-06-04 13:59:07 +0530

    Primitive Data Type in Java

---
+++ Basics Of Java/src/PrimitiveDatatypes.java
@@ -0,0 +1,23 @@
+// Primitive Datatype - They are those data types which are unable to break. 'string' is not a primitive datatype because it is breakable.
+public class PrimitiveDatatypes {
+    public static void main(String[] args) {
+        int n = 123;                     // To store numeric digits
+        // Here, 'n' is identifier. Identifier are the name of variable, method, class or package.
+        // '123' is the literal. literal are the synthetic representation of boolean, char, numeric data etc
+
+
+        float f = 56.23f;                // To store floating numbers
+        double d = 45324.928473298;     // To store decimal numbers which are unable to store in float datatype
+        char ch = 'f';                  //  To store characters
+        long l = 90484098703943L;       // To store numeric digits which are unable to store in int datatype
+        boolean b = 10>1;               // It is used to store true and false values only
+
+        System.out.println(n);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(l);
+        System.out.println(b);
+
+    }
+}
