63e6218af23238e9087f511e1b0763d71325146c
Aditya Yadav <adityayadav44556@gmail.com>
2024-07-10 13:46:55 +0530

    Strings in Java

--- StringInJava/src/Strings.java
+++ StringInJava/src/Strings.java
@@ -14,16 +14,26 @@ public class Strings {
         // Constructor - They are the methods used for creating string objects.
         System.out.println(str2);
 
-        byte B[] = {65, 66, 67, 68};
-        String str3 = new String(B);
+        byte b[] = {65, 66, 67, 68};
+        String str3 = new String(b);
         // new is a keyword used to create object in heap memory.
-        System.out.println(B);
+        System.out.println(str3);
 
         String str4 = new String("Java Programming");
         System.out.println(str4);
 
         // Whenever we apply new the object is created in heap memory and the object literal occupy memory in String constant pool.
         // But the object in string constant pool may or may not be created if it is already present there.
+
+
+        String strX = "java";
+        String strY = "java";
+        // Here, both the reference variable is pointing to same String object in String constant pool. So, the answer will going to be same.
+        System.out.println(strX==strY);
+
+        String strA = "java";  // Here, ref. variable is pointing to java in string pool
+        String strB = new String("java"); // Here, ref. variable is pointing to java in heap memory.
+        System.out.println(strA==strB);   // The result will going to be false
 
 
     }
